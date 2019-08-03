e53d5d12ecba16af2d903f0f82ebd50e5f3d8fa6
pavel <pashkes@gmail.com>
2019-08-03 18:43:47 +0300

    add animation for skills

---
+++ src/components/modal/modal.js
@@ -0,0 +1,50 @@
+import React, { PureComponent } from "react";
+import { TimelineLite } from "gsap";
+import AcrossIcon from "../../images/across.svg";
+
+class Modal extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.myTween = new TimelineLite({ paused: true });
+    this.dialog = null;
+  }
+
+  componentDidMount() {
+    this.myTween
+      .from(this.dialog, 0.35, {autoAlpha: 0, opacity: 0})
+      .play();
+  }
+
+  render() {
+    const {deactivateModal} = this.props;
+    return (
+      <div ref={modal => this.dialog = modal} className={`modal-exp__wrapper`} role="document">
+        <button className="modal-exp__close" id="close-modal" onClick={deactivateModal}>
+          <svg width={18} height={18}>
+            <use xlinkHref={`#${AcrossIcon.id}`}/>
+          </svg>
+        </button>
+        <div className="modal-exp__body">
+          <h2 className="modal-exp__title" id="exp-title">Experience</h2>
+          <ul className="modal-exp__list">
+            <li className="modal-exp__item">
+              <p className="modal-exp__company">Ashmanov &amp;&nbsp;Partners, Moscow (remote)</p>
+              <p className="modal-exp__position">Front-end developer&nbsp;/ 1&nbsp;year 3&nbsp;months</p>
+            </li>
+            <li className="modal-exp__item">
+              <p className="modal-exp__company">MPS Development, Kiev</p>
+              <p className="modal-exp__position">Front-end developer&nbsp;/ 1&nbsp;year 3&nbsp;months</p>
+            </li>
+            <li className="modal-exp__item">
+              <p className="modal-exp__company">Syzygy Technologies Ltd, Kiev</p>
+              <p className="modal-exp__position">Front-end developer&nbsp;/ 4&nbsp;months</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Modal;
--- src/components/skills/skills.js
+++ src/components/skills/skills.js
@@ -1,51 +1,90 @@
-import React from "react";
+import React, { PureComponent } from "react";
+import { TimelineLite } from "gsap";
 
 import "./skills.css";
 
-const Skills = () => (
-  <section className="skills">
-    <h2 className="invisible">Skills</h2>
-    <div className="skills__col">
-      <p className="skills__subtitle">Skills can be&nbsp;taught, personality is&nbsp;inherent. I&nbsp;prefer
-        to&nbsp;keep
-        learning, continue challenging myself, and do&nbsp;interesting things that matter.</p>
-      <ul className="skills__courses">
-        <li>
-          <p className="skills__name">Javascript, Level&nbsp;3 (React.js)</p>
-          <p className="skills__info">HTML Academy&nbsp;/ <a href="https://assets.htmlacademy.ru/certificates/intensive/133/164879.pdf" className="skills__cert-link" target={`blank`} rel="noopener noreferrer" >certificate</a></p>
-        </li>
-        <li>
-          <p className="skills__name">Javascript, Level&nbsp;2</p>
-          <p className="skills__info">HTML Academy&nbsp;/ <a href="https://assets.htmlacademy.ru/certificates/intensive/131/164879.pdf" className="skills__cert-link" target={`blank`} rel="noopener noreferrer" >certificate</a></p>
-        </li>
-        <li>
-          <p className="skills__name">Javascript, Level&nbsp;1</p>
-          <p className="skills__info">HTML Academy&nbsp;/ <a href="https://assets.htmlacademy.ru/certificates/intensive/55/164879.pdf" className="skills__cert-link" target={`blank`} rel="noopener noreferrer" >certificate</a></p>
-        </li>
-        <li>
-          <p className="skills__name">HTML and CSS, Level&nbsp;2</p>
-          <p className="skills__info">HTML Academy / <a href="https://assets.htmlacademy.ru/certificates/intensive/24/164879.pdf" className="skills__cert-link" target={`blank`} rel="noopener noreferrer" >certificate</a></p>
-        </li>
-        <li>
-          <p className="skills__name">HTML and CSS, Level&nbsp;1</p>
-          <p className="skills__info">HTML Academy / <a href="https://assets.htmlacademy.ru/certificates/intensive/18/164879.pdf" className="skills__cert-link" target={`blank`} rel="noopener noreferrer" >certificate</a></p>
-        </li>
-      </ul>
-    </div>
-    <div className="skills__col">
-      <ul className="skills__list">
-        <li>TypeScript</li>
-        <li>React.js</li>
-        <li>Redux</li>
-        <li>CSS</li>
-        <li>JS</li>
-        <li>HTML</li>
-        <li>Enzyme</li>
-        <li>Jest</li>
-        <li>pug</li>
-      </ul>
-    </div>
-  </section>
-)
+const skills = [
+  `TypeScript`,
+  `React`,
+  `Redux`,
+  `CSS`,
+  `JS`,
+  `HTML`,
+  `Enzyme`,
+  `Jest`,
+  `pug`,
+];
 
-export default Skills
+const courses = [
+  {
+    name: `Javascript, Level&nbsp;3 (React.js)`,
+    cert: `https://assets.htmlacademy.ru/certificates/intensive/133/164879.pdf`,
+  },
+  {
+    name: `Javascript, Level&nbsp;2`,
+    cert: `https://assets.htmlacademy.ru/certificates/intensive/131/164879.pdf`,
+  },
+  {
+    name: `Javascript, Level&nbsp;1`,
+    cert: `https://assets.htmlacademy.ru/certificates/intensive/55/164879.pdf`,
+  },
+  {
+    name: `HTML and CSS, Level&nbsp;2`,
+    cert: `https://assets.htmlacademy.ru/certificates/intensive/24/164879.pdf`,
+  },
+  {
+    name: `HTML and CSS, Level&nbsp;1`,
+    cert: `https://assets.htmlacademy.ru/certificates/intensive/18/164879.pdf`,
+  },
+];
+
+class Skills extends PureComponent {
+  constructor(props) {
+    super(props)
+
+    this.courses = [];
+    this.skills = [];
+    this.text = null;
+    this.myTween = new TimelineLite({ paused: true });
+  }
+
+  componentDidMount() {
+    this.myTween
+      .from(this.text, 0.6, {autoAlpha: 0, x: -60})
+      .staggerFrom(this.courses, 0.25, { x: -10, opacity: 0}, 0.1, `-=.3`)
+      .staggerFrom(this.skills, 0.25, { x: -10, opacity: 0}, 0.1)
+      .play();
+  }
+
+  render() {
+    return (
+      <section className="skills">
+        <h2 className="invisible">Skills</h2>
+        <div className="skills__col">
+          <p className="skills__subtitle" ref={p=> this.text = p}>Skills can be&nbsp;taught, personality is&nbsp;inherent. I&nbsp;prefer
+            to&nbsp;keep
+            learning, continue challenging myself, and do&nbsp;interesting things that matter.</p>
+          <ul className="skills__courses">
+            {
+              courses.map(({ name, cert }, index) => (
+                <li key={name} ref={course => this.courses[index] = course}>
+                  <p className="skills__name">{name}</p>
+                  <p className="skills__info">HTML Academy&nbsp;/ <a href={cert} className="skills__cert-link" target={`blank`} rel="noopener noreferrer">certificate</a></p>
+                </li>
+              ))
+            }
+          </ul>
+        </div>
+        <div className="skills__col">
+          <ul className="skills__list">
+            {
+              skills.map((item, index) => <li ref={skill => this.skills[index] = skill} key={item}>{item}</li>)
+            }
+          </ul>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Skills;
