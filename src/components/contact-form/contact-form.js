3008edbeb2004dd89b78e663abfc03186c0261b2
pavel <pashkes@gmail.com>
2019-08-06 20:09:05 +0300

    is added validation form

--- src/components/contact-form/contact-form.js
+++ src/components/contact-form/contact-form.js
@@ -1,21 +1,30 @@
-import React, { useEffect, useRef } from "react"
-import compose from "recompose/compose"
+import React, { useEffect, useRef, useState } from "react"
+import { withFormik, Form, Field, ErrorMessage } from "formik"
+import * as Yup from "yup"
+import { navigate } from "gatsby"
 
 import "./contact-form.css"
 
-import Field from "../field/field"
 
-import withContactForm from "./with-contact-form"
-import withSubmittingForm from "./with-submitting-form"
 import { TimelineLite } from "gsap"
 
-const ContactForm = (props) => {
-  const { name, email, message, onChange, isSending, error, onSubmit } = props
+const ERROR = `Something went wrong, check filled data`
+
+const encode = (data) => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&")
+}
+const ContactForm = ({ values, errors, isSubmitting, touched }) => {
   let nameRef = useRef(null)
   let emailRef = useRef(null)
   let messageRef = useRef(null)
   let buttonRef = useRef(null)
   const myTween = new TimelineLite({ paused: true })
+  const [isFilledName, setFillName] = useState(false)
+  const [isFilledEmail, setFillEmail] = useState(false)
+  const [isFilledMessage, setFillMessage] = useState(false)
+
   useEffect(() => {
     const setFocusInputName = () => nameRef.current.querySelector(`input`).focus()
     myTween
@@ -24,64 +33,102 @@ const ContactForm = (props) => {
       .to(messageRef.current, .25, { opacity: 1, scaleX: 1, transformOrigin: "0 0" }, "-=0.1")
       .to(buttonRef.current, .25, { opacity: 1, y: 0 })
       .play()
-      .eventCallback(`onComplete`, () => setFocusInputName())
-      return () => myTween.eventCallback('onComplete', null)
-  }, )
+    .eventCallback(`onComplete`, () => setFocusInputName())
+    return () => myTween.eventCallback("onComplete", null)
+
+    if (values.name && values.name.length > 0) {
+      setFillName(true)
+    } else {
+      setFillName(false)
+    }
+
+    if (values.email && values.email.length > 0) {
+      setFillEmail(true)
+    } else {
+      setFillEmail(false)
+    }
+
+    if (values.message && values.message.length > 0) {
+      setFillMessage(true)
+    } else {
+      setFillMessage(false)
+    }
+  }, [values.name, values.email, values.message])
 
   return (
-    <form className="contact-form" onSubmit={onSubmit} id="contacts" action={`/success-sent/`}
+    <Form className="contact-form"
+          action={`/success-sent/`}
           name="contact"
           method="POST"
           data-netlify="true">
       <input type="hidden" name="form-name" value="contact"/>
       <p hidden><label>Don’t fill this out: <input name="bot-field"/></label></p>
+
       <div className="contact-form__body">
         <div className="contact-form__fields">
-          <Field
-            id={`contacts-name`}
-            onChange={onChange}
-            label={`Name`}
-            value={name}
-            name={`name`}
-            isRequired={true}
-            ref={nameRef}
-            className={`scale-to-right`}
-          />
-          <Field
-            onChange={onChange}
-            id={`contacts-email`}
-            value={email}
-            name={`email`}
-            label={`E-mail`}
-            isRequired={true}
-            type={`email`}
-            pattern={`[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$`}
-            inputMode="email"
-            ref={emailRef}
-            className={`scale-to-right`}
-          />
+          <div
+            className={`form-control scale-to-right ${isFilledName ? `filled` : ``} ${errors.name && touched.name ? `error` : ``}`}
+            ref={nameRef}>
+            <Field className={`form-control__field`} name="name" id="contacts-name"/>
+            <label className="form-control__label" htmlFor="contacts-name">Name</label>
+            <ErrorMessage className="form-control__error" component="span" name="name"/>
+          </div>
+          <div
+            className={`form-control scale-to-right ${isFilledEmail ? `filled` : ``} ${errors.email && touched.email ? `error` : ``}`}
+            ref={emailRef}>
+            <Field type="email" className={`form-control__field`} name="email" id="contacts-email"
+                   pattern={`[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$`} inputMode="email"/>
+            <label className="form-control__label" htmlFor="contacts-email">E-mail</label>
+            <ErrorMessage className="form-control__error" component="span" name="email"/>
+          </div>
         </div>
-        <Field
-          id={`contacts-message`}
-          onChange={onChange}
-          value={message}
-          name={`message`}
-          label={`What I need to know?`}
-          ref={messageRef}
-          className={`scale-to-right`}
-        />
+        <div
+          className={`form-control scale-to-right ${isFilledMessage ? `filled` : ``} ${errors.message && touched.message ? `error` : ``}`}
+          ref={messageRef}>
+          <Field className={`form-control__field`} name="message" id="contacts-message"/>
+          <label className="form-control__label" htmlFor="contacts-message">What I need to know?</label>
+        </div>
       </div>
-      {error && <span aria-live="assertive" role="banner">{error}</span>}
       <button
         ref={buttonRef}
-        className={`contact-form__submit button slide-down ${isSending && `button--disabled`}`}
-        disabled={isSending}
-        type="submit">{isSending ? `Sending...` : `Get in touch`}</button>
-    </form>
+        className={`contact-form__submit button slide-down ${isSubmitting && `button--disabled`}`}
+        disabled={isSubmitting}
+        type="submit">{isSubmitting ? `Sending...` : `Get in touch`}</button>
+    </Form>
   )
 }
-const enhance = compose(
-  withContactForm,
-  withSubmittingForm,
-)
-export default enhance(ContactForm)
+const ValidationContactForm = withFormik({
+  mapPropsToValues({ email, name, message }) {
+    return {
+      email: email || ``,
+      name: name || ``,
+      message: message || ``,
+    }
+  },
+  handleSubmit(values, { setError, setSubmitting }) {
+    setSubmitting(true)
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...values }),
+    })
+      .then(() => {
+        setSubmitting(false)
+        navigate("/success-sent/")
+      })
+      .catch(() => {
+        setSubmitting(false)
+        setError(ERROR)
+      })
+  },
+  validationSchema: Yup.object().shape({
+    name: Yup.string()
+      .min(3, `Name must be at least 3 characters`)
+      .required(`Name is a required field`),
+    email: Yup.string()
+      .email(`Please enter a valid email address`)
+      .required(`Email is a required field`),
+  }),
+})(ContactForm)
+
+export default ValidationContactForm
