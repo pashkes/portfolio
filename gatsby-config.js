f91b59884e8819ac465ffa2c42e9a6f2b6b3f161
pavel <pashkes@gmail.com>
2019-08-17 14:59:00 +0300

    is added favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
         background_color: `#101626`,
         theme_color: `#92d79a`,
         display: `standalone`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-offline`,
