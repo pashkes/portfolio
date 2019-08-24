b93d549c63f7ca4d35f0b9e6db709505498df1b6
pavel <pashkes@gmail.com>
2019-08-24 22:06:26 +0300

    changed places script of analytic

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,7 +53,7 @@ module.exports = {
       options: {
         trackingId: "UA-146310499-1",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
+        head: true,
         // Setting this parameter is optional
         anonymize: true,
         // Setting this parameter is also optional
