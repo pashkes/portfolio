31f7fe21e02e2c108efb2d7e87b86cd5bae56add
Pavel Tarasenko <pashkes@gmail.com>
2020-08-18 15:13:51 +0300

    update GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,7 +51,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "UA-146310499-1",
+        trackingId: "UA-107002323-1",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: true,
         // Setting this parameter is optional
